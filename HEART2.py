dc40ab1235f081a502b117879052a357e48bd51e
rivaner91-sudo <rivaner91@gmail.com>
2026-08-10 15:33:57 -0600

    Fix drop column syntax in HEART2.py

--- HEART2.py
+++ HEART2.py
@@ -53,7 +53,7 @@ df = user_input_features()
 
 HEART2 =  pd.read_csv('HEART2.csv', encoding='latin-1')
 #df= df.drop(columns=['target'])
-X = df.drop(column='target')
+X = df.drop(columns='target')
 Y = df['target']
 
 
