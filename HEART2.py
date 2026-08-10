e43a28b08ecc2bc77ba78cecd5e1ced9bff062f5
rivaner91-sudo <rivaner91@gmail.com>
2026-08-10 15:25:55 -0600

    Fix target variable assignment in HEART2.py

--- HEART2.py
+++ HEART2.py
@@ -54,7 +54,7 @@ df = user_input_features()
 HEART2 =  pd.read_csv('HEART2.csv', encoding='latin-1')
 #df= df.drop(columns=['target'])
 X = df.drop(column='target', axis=1)
-Y = df[target]
+Y = df['target']
 
 
 classifier = DecisionTreeClassifier(max_depth=5, criterion='entropy', min_samples_leaf=10, max_features=5, random_state=0)
